7d8e40646e0cc442398fca6934fb28c6108c1164
fsimoes <francisconfqsimoes@gmail.com>
2020-07-07 15:59:16 +0100

    Added zathurarc and continued with manjaro_install_commands.sh.

--- manjaro_client/manjaro_install_commands.sh
+++ manjaro_client/manjaro_install_commands.sh
@@ -11,41 +11,25 @@ yay -S luke-st-git #Install Luke Smith's version of suckless's simple terminal (
 pacman -S dmenu #necessary for some st funcitonalities.
 yay -S ytop #Great terminal graphical system monitor.
 
-# Tweak awesome's config
-cp ~/.config/awesome/rc.lua ~/.config/awesome/rc.lua_backup #Create backup of the config file.
-cp -R /usr/share/awesome/themes ~/.config/awesome/themes #Copy all default themes to awesomes's config dir.
-#sed --in-place 's#beautiful.init("~/.config/awesome/themes/cesious/theme.lua")#beautiful.init("~/.config/awesome/themes/default/theme.lua")#' ~/.config/awesome/rc.lua #Use default theme instead of cesious theme.
-mkdir ~/Pictures/Wallpapers
-cp -R /usr/share/backgrounds/manjaro-wallpapers-18.0 ~/Pictures/Wallpapers/manjaro18 #Copy manjaro wallpapers to created folder.
-#sed --in-place 's#theme.wallpaper = themes_path.."default/background.png"#theme.wallpaper = "~/Pictures/Wallpapers/manjaro18/illyria-default2.jpg"#' ~/.config/awesome/themes/default/theme.lua #Change wallpaper to a nice one from manjaro18.
-#sed --in-place 's#properties = { titlebars_enabled = true }#properties = { titlebars_enabled = false }#' ~/.config/awesome/rc.lua #Disable title bars.
-#sed --in-place 's/theme.border_focus  = "#535d6c"/theme.border_focus  = "#00FF00"/' ~/.config/awesome/themes/default/theme.lua #Change active border's color to lime. 
-#sed --in-place 's/theme.border_width  = dpi(1)/theme.border_width  = dpi(2)/' ~/.config/awesome/themes/default/theme.lua #Makes border thicker.
-#sed --in-place 's/theme.fg_focus      = "#ffffff"/theme.fg_focus    = "#00FF00"/' ~/.config/awesome/themes/default/theme.lua #Changes font color to lime in tags.
-#sed --in-place 's/theme.border_normal = "#000000"/theme.border_normal = "#454545"/' ~/.config/awesome/themes/default/theme.lua #Changes borders of unfocused windows to gray.
-## Set Logo as Manjaro logo, as in the cesious theme.
-cp ~/awesome/themes/cesious/icons/manjaro64.png ~/awesome/themes/default/manjaro64.png
-#sed --in-place '123d' ~/.config/awesome/themes/default/theme.lua
-#sed --in-place '122d' ~/.config/awesome/themes/default/theme.lua
-#sed --in-place '121d' ~/.config/awesome/themes/default/theme.lua
-#sed --in-place '121i theme.awesome_icon = "~/.config/awesome/themes/default/manjaro64.png"' ~/.config/awesome/themes/default/theme.lua
-## Add keybinding to toggle wibar visibility.
-#sed --in-place '266i -- START (simoes)' ~/.config/awesome/rc.lua
-#sed --in-place '267i awful.key({ modkey }, ";", function ()' ~/.config/awesome/rc.lua
-#sed --in-place '268i myscreen = awful.screen.focused()' ~/.config/awesome/rc.lua
-#sed --in-place '269i myscreen.mywibox.visible = not myscreen.mywibox.visible' ~/.config/awesome/rc.lua
-#sed --in-place '270i end,' ~/.config/awesome/rc.lua
-#sed --in-place '271i {description = "toggle statusbar"}' ~/.config/awesome/rc.lua
-#sed --in-place '272i ),' ~/.config/awesome/rc.lua
-#sed --in-place '273i -- END (simoes)' ~/.config/awesome/rc.lua
-## Comment out useless layouts.
-#sed --in-place 's/awful.layout.suit.fair.horizontal,/--awful.layout.suit.fair.horizontal,/' ~/.config/awesome/rc.lua 
-#sed --in-place 's/awful.layout.suit.floating,/--awful.layout.suit.floating/' ~/.config/awesome/rc.lua 
 
-# Dotfiles and config
+##############################
+# Dotfiles and configs
 #Pull from Github repo:
 mkdir ~/Documents/dotfiles
 git clone https://github.com/francisco-simoes/dotfiles ~/Documents/dotfiles
+
+## AwesomeWM
+#Backup awesome's config and tweak a few things.
+mv ~/.config/awesome/rc.lua ~/.config/awesome/rc.lua_backup #Create backup of the config file.
+mv -R /usr/share/awesome/themes ~/.config/awesome/themes #Copy all default themes to awesomes's config dir.
+mkdir ~/Pictures/Wallpapers
+cp -R /usr/share/backgrounds/manjaro-wallpapers-18.0 ~/Pictures/Wallpapers/manjaro18 #Copy manjaro wallpapers to created folder.
+#Set Logo as Manjaro logo, as in the cesious theme.
+cp ~/awesome/themes/cesious/icons/manjaro64.png ~/awesome/themes/default/manjaro64.png
+#Get config files
+cp ~/Documents/dotfiles/manjaro_client/rc.lua ~/.config/awesome
+cp ~/Documents/dotfiles/manjaro_client/theme.lua ~/.config/awesome/themes/default
+
 ## Neovim
 mkdir ~/.config/nvim
 cp ~/Documents/dotfiles/init.vim ~/.config/nvim/init.vim
@@ -55,11 +39,34 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 #Install nodejs and npm for coc-vim and coc-python:
 pacman -S nodejs npm #If this fails, do `curl -sL install-node.now.sh/lts | bash` as suggested on github.
 lxterminal --command 'nvim "+CocInstall coc-python"' #Installs coc-python. Assumes you have lxtermial.
+
+## Ranger
+mkdir ~/.config/ranger
+cp ~/Documents/dotfiles/ranger/rc.conf ~/.config/ranger
+cp ~/Documents/dotfiles/ranger/rifle.conf ~/.config/ranger
+cp ~/Documents/dotfiles/ranger/scope.sh ~/.config/ranger
+
+## Zathura
+mkdir ~/.config/zathura
+cp ~/Documents/dotfiles/zathurarc ~/.config/zathura
+
 ## zsh
 mv ~/.zshrc ~/.zshrc_backup
-cp ~/Documents/dotfiles/.zshrc ~/.zshrc
-## .Xresources, used by simple terminal.
-mv ~/Documents/dotfiles/manjaro_client/.Xresources ~
+cp ~/Documents/dotfiles/zsh/.zshrc ~/.zshrc
+
+## .Xresources, used to customize simple terminal.
+cp ~/Documents/dotfiles/manjaro_client/.Xresources ~
+
+## Dotfile backups with rsync
+mkdir ~/.scripts
+cp ~/Documents/dotfiles/manjaro_client/rsync_dotfiles.sh ~/.scripts
+cp ~/Documents/dotfiles/manjaro_client/rsync_dotfiles_and_git_push.sh ~/.scripts
+cp ~/Documents/dotfiles/manjaro_client/rsync_dotfiles_zsh.txt ~/Documents
+cp ~/Documents/dotfiles/manjaro_client/rsync_dotfiles_ranger.txt ~/Documents
+cp ~/Documents/dotfiles/manjaro_client/rsync_dotfiles_manjaro.txt ~/Documents
+cp ~/Documents/dotfiles/manjaro_client/rsync_dotfiles_general.txt ~/Documents
+
+##############################
 
 # Miscellaneous software
 pacman -S screenkey
